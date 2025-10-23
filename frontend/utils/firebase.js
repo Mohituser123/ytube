563bc0d0d35f2ea393a07655dcd766ce406a5f22
Mohit Patil <167691266+Mohituser123@users.noreply.github.com>
2025-10-23 17:59:13 +0530

    Update firebase.js

--- frontend/utils/firebase.js
+++ frontend/utils/firebase.js
@@ -6,12 +6,13 @@ import {getAuth, GoogleAuthProvider} from "firebase/auth"
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
-  authDomain: "ytube-clone-f74bc.firebaseapp.com",
-  projectId: "ytube-clone-f74bc",
-  storageBucket: "ytube-clone-f74bc.firebasestorage.app",
-  messagingSenderId: "978088294555",
-  appId: "1:978088294555:web:25a1819ee6199b265cae3e"
+  authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_FIREBASE_APPID
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
